150146db85a3f04142bc349f8162a36b9780d640
marksantander <msntndr.ms@gmail.com>
2023-09-26 20:02:47 -0400

    added workbox plugins for a service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,35 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src-sw.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js',
+      }),
+    ],
+
+    module: {
+      rules: [
+        
+      ],
+    },
+  };
+};
+
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
